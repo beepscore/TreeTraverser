b8d11598892b5178a3d78e5951cb4ab7862f12e8
beepscore <support@beepscore.com>
2015-06-04 15:09:30 -0700

    In TreeTraverser add debug log. Now in Android Studio logcat shows node traversal path.

--- app/src/main/java/com/beepscore/android/treetraverser/TreeTraverser.java
+++ app/src/main/java/com/beepscore/android/treetraverser/TreeTraverser.java
@@ -1,14 +1,20 @@
 package com.beepscore.android.treetraverser;
 
+import android.util.Log;
+
 /**
  * Created by stevebaker on 6/4/15.
  */
 public class TreeTraverser {
 
     public boolean isNameInTree(String name, Node node) {
+
         if (node == null) {
             return false;
         }
+
+        Log.d("isNameInTree", node.toString());
+
         // "pre-order" check node before subtrees
         if (node.name == name) {
             return true;
